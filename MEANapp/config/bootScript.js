ca57cdc6a1f52a930954ce9d31806bc387b43b27
Angel-Minguez <sindariel.eq@gmail.com>
2017-05-08 20:49:55 +0200

    v0.0.3

--- MEANapp/config/bootScript.js
+++ MEANapp/config/bootScript.js
@@ -5,19 +5,38 @@
 const debug = require('debug')('bootScript');
 const log = require('log');
 const fs = require('fs');
-var bootLog = new log('info', fs.createWriteStream(__dirname + '/../logs/bootLog.txt')); 
-bootLog.info('bootScript launched with %s at %s', process.argv.shift(), process.argv.shift());
+const stream = require('stream');
+var logStream = fs.createWriteStream(__dirname + '/../logs/bootLog.txt',{ flags:"a" });
+var passStream = new stream.PassThrough();
+//var bootLog = new log('info', passStream);
+var logger = new console.Console(passStream);
+passStream.pipe(process.stdout);
+passStream.pipe(logStream);
+//bootLog.info('\nbootScript launched with %s at %s', process.argv.shift(), process.argv.shift());
 var envRegExp = new RegExp(/^(--env=)(developement|production)/);
 var portRegExp = new RegExp(/^(--port=)([0-9]{2,5}$)/);
+var debugRegExp = new RegExp(/^(--debug=)([a-z]{1,100}$)/);
 process.env.NODE_ENV = process.env.npm_package_config_environment || 'developement';
-console.log(process.argv);
-process.argv.forEach((_arg, _index) => {
-    if (envRegExp.test(_arg)) process.env.NODE_ENV = RegExp.$2;
-    else console.log("Error");
-    if (portRegExp.test(_arg)) process.env.PORT = RegExp.$2;
-    else console.log("Error");
-});
-console.log(process.env.NODE_ENV);
-console.log(process.env.PORT);
-console.log(process.env.npm_package_config_environment);
+logger.log(".uiyuryrtyetet");
+if(!process.argv.every((_arg, _index) => {
+	if (envRegExp.test(_arg)) {
+		//process.env.NODE_ENV = envRegExp.exec(_arg)[2];
+		//console.log("Environment set: %s", process.env.NODE_ENV);
+		bootLog.info("\nEnvironment set: %s", process.env.NODE_ENV);
+		return true;
+	}
+    else if (portRegExp.test(_arg)) { 
+			process.env.PORT = portRegExp.exec(_arg)[2];
+			return true;
+		}
+		else if (debugRegExp.test(_arg)) {
+				process.env.DEBUG = debug.exec(_arg)[2];
+				return true;
+			}
+	//console.log('Wrong parameter format at parameter ' + _index);
+	return false;
+})) console.log('Incorrect arguments please use --h for help.');
+//console.log(process.env.NODE_ENV);
+//console.log(process.env.PORT);
+//console.log(process.argv);
 require('../server.js');
