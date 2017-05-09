485deee7afd41432c448b4cb537c31367421001c
Angel-Minguez <sindariel.eq@gmail.com>
2017-05-09 15:46:43 +0200

    v0.0.3

--- MEANapp/config/bootScript.js
+++ MEANapp/config/bootScript.js
@@ -1,23 +1,14 @@
 ﻿/******************************************************************************************************/
-/*      Modulo de control del servidor                                                                */
+/*      Script de arranque e inicializacion de variables de entorno                                   */
 /*      Autor: Angel Minguez Burillo                                                                  */
+/*      Fecha: 7/5/2017                                                                               */
 /******************************************************************************************************/
-const debug = require('debug')('bootScript');
-const log = require('log');
-const fs = require('fs');
-const stream = require('stream');
-var logStream = fs.createWriteStream(__dirname + '/../logs/bootLog.txt',{ flags:"a" });
-var passStream = new stream.PassThrough();
-//var bootLog = new log('info', passStream);
-var logger = new console.Console(passStream);
-passStream.pipe(process.stdout);
-passStream.pipe(logStream);
-//bootLog.info('\nbootScript launched with %s at %s', process.argv.shift(), process.argv.shift());
+const logger = require('./logger.js')(__dirname + '/logs/bootLog.txt', { flags: 'a+', silent: false});
 var envRegExp = new RegExp(/^(--env=)(developement|production)/);
 var portRegExp = new RegExp(/^(--port=)([0-9]{2,5}$)/);
 var debugRegExp = new RegExp(/^(--debug=)([a-z]{1,100}$)/);
 process.env.NODE_ENV = process.env.npm_package_config_environment || 'developement';
-logger.log(".uiyuryrtyetet");
+logger.log("testeo");
 if(!process.argv.every((_arg, _index) => {
 	if (envRegExp.test(_arg)) {
 		//process.env.NODE_ENV = envRegExp.exec(_arg)[2];
@@ -35,7 +26,7 @@ if(!process.argv.every((_arg, _index) => {
 			}
 	//console.log('Wrong parameter format at parameter ' + _index);
 	return false;
-})) console.log('Incorrect arguments please use --h for help.');
+})) logger.log('Incorrect arguments please use --h for help.');
 //console.log(process.env.NODE_ENV);
 //console.log(process.env.PORT);
 //console.log(process.argv);
---
+++ MEANapp/config/logger.js
@@ -0,0 +1,41 @@
+﻿/******************************************************************************************************/
+/*      Modulo para la funcion logger que genera archivos de log                                      */
+/*      Autor: Angel Minguez Burillo                                                                  */
+/*      Fecha: 8/5/2017                                                                               */
+/******************************************************************************************************/
+'use strict'
+const path = require('path');                                                                           //Modulo para manejo de paths
+const stream = require('stream').Writable/*.Duplex;   */                                                                    //Requerimos el modulo de control de streams
+const fs = require('fs');                                                                               //Modulo de control de ficheros
+
+module.exports = function (_path, options) {                                                            //Como parametros usamos la ruta al archivo de log y las flags de acceso al archivo
+    //if (!options.silent) /*passStream*/logger.pipe(process.stdout);           
+
+
+
+
+    let fileStream = fs.createWriteStream(path.relative(__dirname, _path), { flags: "r+", start: 0 });  
+    class duplexLogger extends stream {
+        constructor(config) {
+            super(config);
+        }
+        /*_read(size) { return super.read(size); }*/
+        _write(chunk, encoding, callback) {
+            var date = new Date();
+            var str = chunk.toString() + date.toISOString();
+            /*return super.write(str, 'utf8', callback);*/
+
+
+            fileStream.write(str, 'utf8');
+            process.stdout.write(str, 'utf8');
+          //  callback();
+        }
+    }
+    var logger = new duplexLogger();
+    //Creamos un stream duplex que actua como stream de lectura y escritura
+
+     //Creamos el stream de escritura que escribira en el archivo de log
+                                        //Enlazamos lo que se escriba en el stream duplex a la salida estandar (pantalla)
+    /*passStream*//*logger.pipe(fileStream);  */                                                                      //Enlazamos lo que se escriba en el stream duplex al stream que escribe en el log
+    return new console.Console(/*passStream*/logger );                                                             //Creamos una nueva consola que recibira lo mensajes
+}
