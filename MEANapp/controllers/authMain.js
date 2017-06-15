af1edd80915608cab13a695aae34b73b6796ec84
Angel-Minguez <sindariel.eq@gmail.com>
2017-06-15 15:31:48 +0200

    v0.3.1.1

--- MEANapp/controllers/authMain.js
+++ MEANapp/controllers/authMain.js
@@ -9,32 +9,37 @@ const path = require('path');                                   //Modulo de mane
 const debug = require('debug')('authMain');                 	//Modulo de mensajes de debug
 const jwt = require('jsonwebtoken');                            //Requerimos el modulo jwt para verificar el token
 module.exports = function (req, res, next) {                    //Funcion exportada
-	jwt.verify(req.body.token, 'my_secret', (_err, _decoded) => {   //Verificamos el token que se manda al lanzar la app angular
-        if (_err) {                                                 //Si hay un error en la verificacion
-            debug(_err.message);                                    //Mostramos mensaje de debug
-            res.json({                                              //Enviamos respuesta a angular con el error
-                userName: 'N/A',
-                authResult: 'AUTH_ERROR',
-                authError: _err.message
-            });
-        }
-        else if (_decoded && req.session.userName == _decoded.userName) {   //Si el usuario del JWT y el de la sesion coinciden
-            debug('Authentication successful! %s', _decoded.userName);      //Mostramos mensaje de exito
-            res.json({                                                      //Enviamos a angular respuesta positiva de la autenticacion
-                userName: _decoded.userName,
-                authResult: 'AUTH_OK',
-                authError: 'N/A'
-            });
-        }
-        else {                                                                          //Si la sesion ha expirado o el token no es valido
-            debug('Authentication failed! [%s] session expired', _decoded.userName);    //Mostramos mensaje de error
-            res.json({                                                                  //Enviamos respuesta a angular con el error
-                userName: _decoded.userName,
-                authResult: 'AUTH_ERROR',
-                authError: 'Session expired!'
-            });
-        }
-	});
+    if (req.body.token) {
+        jwt.verify(req.body.token, 'my_secret', (_err, _decoded) => {   //Verificamos el token que se manda al lanzar la app angular
+            if (_err) {                                                 //Si hay un error en la verificacion
+                debug(_err.message);                                    //Mostramos mensaje de debug
+                res.json({                                              //Enviamos respuesta a angular con el error
+                    userName: 'N/A',
+                    authResult: 'AUTH_ERROR',
+                    authError: _err.message
+                });
+            }
+            else if (_decoded && req.session.userName == _decoded.userName) {   //Si el usuario del JWT y el de la sesion coinciden
+                debug('Authentication successful! %s', _decoded.userName);      //Mostramos mensaje de exito
+                res.json({                                                      //Enviamos a angular respuesta positiva de la autenticacion
+                    userName: _decoded.userName,
+                    authResult: 'AUTH_OK',
+                    authError: 'N/A'
+                });
+            }
+            else {                                                                          //Si la sesion ha expirado o el token no es valido
+                debug('Authentication failed! [%s] session expired', _decoded.userName);    //Mostramos mensaje de error
+                res.json({                                                                  //Enviamos respuesta a angular con el error
+                    userName: _decoded.userName,
+                    authResult: 'AUTH_ERROR',
+                    authError: 'Session expired!'
+                });
+            }
+        });
+    } else if (req.body.randomString) {
+
+
+    }
 }
 /******************************************************************************************************/
 /*      Requerido por /routes/indexRoute.js                                                           */
